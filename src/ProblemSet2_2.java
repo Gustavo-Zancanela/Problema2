283f02a7aa6bce475a3885fb1d78156b43a911ba
ALUNO <ALUNO@LAB04-PC32>
2024-06-05 19:49:14 -0300

    Commit project Problema2

---
+++ src/ProblemSet2_2.java
@@ -0,0 +1,28 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        System.out.println("                XXXX  XXXXXX    ");
+        System.out.println("    XXXXXX    XX@@@@XX      XX  ");
+        System.out.println("  XX      XXXX@@@@@@XXXX  XXXX  ");
+        System.out.println("  XX        XX@@XXXX@@@@XX@@@@XX");
+        System.out.println("  XX        XX@@@@XX@@@@XXXX@@XX");
+        System.out.println("  XX          XXXX  XXXX@@XX@@XX");
+        System.out.println("XX                    XX@@@@XXXX");
+        System.out.println("XX                      XXXX  XX");
+        System.out.println("XXXX                        XXXX");
+        System.out.println("XX      XX            XX      XX");
+        System.out.println("XXXX    XX            XX    XXXX");
+        System.out.println("XX            XXXX            XX");
+        System.out.println("  XX                        XX  ");
+        System.out.println("  XX                        XX  ");
+        System.out.println("    XXXX                XXXX    ");
+        System.out.println("        XXXXXXXXXXXXXXXX        ");
+        
+        //hello kitty
+        
+        
+        
+        
+    } 
+}
